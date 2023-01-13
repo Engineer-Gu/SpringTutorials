571d40d8089a0d26bb069737af7ec4001cde673b
laogu <1437594522@qq.com>
2023-01-13 20:15:25 +0800

    实例化策略接口，在构造函数instantiate中添加必要的入参信息

---
+++ spring-step-03/src/main/java/com/lovegu/springframework/factory/support/InstantiationStrategy.java
@@ -0,0 +1,17 @@
+package com.lovegu.springframework.factory.support;
+
+import com.lovegu.springframework.BeansException;
+import com.lovegu.springframework.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @author 老顾
+ * @description 实例化策略接口
+ * @date 2023/1/13
+ */
+public interface InstantiationStrategy {
+
+    Object instantiate(BeanDefinition beanDefinition, String beanName,
+                       Constructor ctor, Object[] args) throws BeansException;
+}
