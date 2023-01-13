e00ffc750f71abb735b8a770e44c12c05fa4e6dc
laogu <1437594522@qq.com>
2023-01-13 20:47:25 +0800

    添加变量 name，生成了构造方法和重写了 toString()方法

--- spring-step-03/src/test/java/com/lovegu/springframework/bean/UserService.java
+++ spring-step-03/src/test/java/com/lovegu/springframework/bean/UserService.java
@@ -7,7 +7,20 @@ package com.lovegu.springframework.bean;
  */
 public class UserService {
 
+    private String name;
+
+    public UserService(String name) {
+        this.name = name;
+    }
+
     public void queryUserInfo() {
         System.out.println("查询用户信息");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("");
+        stringBuilder.append("").append(name);
+        return stringBuilder.toString();
     }
 }
