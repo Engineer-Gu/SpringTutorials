610d73876ffd21e808a0178cdafe06e721913849
laogu <1437594522@qq.com>
2023-01-14 20:15:53 +0800

    补全 bean 对象的定义

--- spring-step-04/src/main/java/com/lovegu/springframework/factory/config/BeanDefinition.java
+++ spring-step-04/src/main/java/com/lovegu/springframework/factory/config/BeanDefinition.java
@@ -1,5 +1,7 @@
 package com.lovegu.springframework.factory.config;
 
+import com.lovegu.springframework.PropertyValues;
+
 /**
  * @author 老顾
  * @description 将 Bean 对象的实例化放到容器中处理
@@ -9,8 +11,15 @@ public class BeanDefinition {
 
     private Class beanClass;
 
+    private final PropertyValues propertyValues;
     public BeanDefinition(Class beanClass) {
         this.beanClass = beanClass;
+        this.propertyValues = new PropertyValues();
+    }
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues !=null ?propertyValues : new PropertyValues();
     }
 
     public Class getBeanClass() {
@@ -19,5 +28,9 @@ public class BeanDefinition {
 
     public void setBeanClass(Class beanClass) {
         this.beanClass = beanClass;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
     }
 }
