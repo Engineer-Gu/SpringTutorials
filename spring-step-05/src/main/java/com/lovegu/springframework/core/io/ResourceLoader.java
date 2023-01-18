0cd1af62846352ea85cb06c0416b2d6bdcb9061e
laogu <1437594522@qq.com>
2023-01-18 21:19:01 +0800

    定义获取资源的接口，在接口中传递资源地址

---
+++ spring-step-05/src/main/java/com/lovegu/springframework/core/io/ResourceLoader.java
@@ -0,0 +1,13 @@
+package com.lovegu.springframework.core.io;
+
+/**
+ * @author 老顾
+ * @description 定义获取资源的接口，在接口中传递资源地址
+ * @date 2023/1/18
+ */
+public interface ResourceLoader {
+
+    String CLASSPATH_URL_PREFIX = "classpath:";
+
+    Resource getResource(String location);
+}
