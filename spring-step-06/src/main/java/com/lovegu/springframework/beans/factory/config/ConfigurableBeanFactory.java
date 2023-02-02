7f99711349ee1721b29e15f247b311c0d09957cb
laogu <1437594522@qq.com>
2023-02-02 10:29:46 +0800

    由大多数 bean 工厂实现的配置接口

---
+++ spring-step-06/src/main/java/com/lovegu/springframework/beans/factory/config/ConfigurableBeanFactory.java
@@ -0,0 +1,17 @@
+package com.lovegu.springframework.beans.factory.config;
+
+
+import com.lovegu.springframework.beans.factory.HierarchicalBeanFactory;
+
+/**
+ * 由大多数 bean 工厂实现的配置接口
+ */
+public interface ConfigurableBeanFactory extends HierarchicalBeanFactory, SingletonBeanRegistry {
+
+    String SCOPE_SINGLETON = "singleton";
+
+    String SCOPE_PROTOTYPE = "prototype";
+
+    void addBeanPostProcessor(BeanPostProcessor beanPostProcessor);
+
+}
