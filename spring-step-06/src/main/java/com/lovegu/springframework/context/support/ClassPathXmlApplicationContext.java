ba9e2ccc94de35ad49bea26d6dd036d1cec4badd
laogu <1437594522@qq.com>
2023-02-02 16:20:56 +0800

    应用上下文实现类-具体对外给用户提供的应用上下文类

---
+++ spring-step-06/src/main/java/com/lovegu/springframework/context/support/ClassPathXmlApplicationContext.java
@@ -0,0 +1,43 @@
+package com.lovegu.springframework.context.support;
+
+import com.lovegu.springframework.beans.BeansException;
+
+/**
+ * @author 老顾
+ * @description 应用上下文实现类-具体对外给用户提供的应用上下文类
+ * @date 2023/2/2
+ */
+public abstract class ClassPathXmlApplicationContext extends AbstractXmlApplicationContext {
+
+    private String[] configLocations;
+
+    public ClassPathXmlApplicationContext() {
+    }
+
+    /**
+     * 从 XML 中加载 BeanDefinition，并刷新上下文
+     *
+     * @param configLocations
+     * @throws BeansException
+     */
+    public ClassPathXmlApplicationContext(String configLocations) throws BeansException {
+        this(new String[]{configLocations});
+    }
+
+    /**
+     * 从 XML 中加载 BeanDefinition，并刷新上下文
+     *
+     * @param configLocations
+     * @throws BeansException
+     */
+    public ClassPathXmlApplicationContext(String[] configLocations) throws BeansException {
+        this.configLocations = configLocations;
+        refresh();
+    }
+
+    @Override
+    protected String[] getConfigLocations() {
+        return configLocations;
+    }
+
+}
