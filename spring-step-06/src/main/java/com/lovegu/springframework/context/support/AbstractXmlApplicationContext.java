a4f456b636613aeda0793e73eb4743e1e17bd8ac
laogu <1437594522@qq.com>
2023-02-02 16:12:59 +0800

    上下文中对配置信息的加载

---
+++ spring-step-06/src/main/java/com/lovegu/springframework/context/support/AbstractXmlApplicationContext.java
@@ -0,0 +1,25 @@
+package com.lovegu.springframework.context.support;
+
+import com.lovegu.springframework.beans.factory.support.DefaultListableBeanFactory;
+import com.lovegu.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import java.util.Arrays;
+
+/**
+ * @author 老顾
+ * @description 上下文中对配置信息的加载
+ * @date 2023/2/2
+ */
+public abstract class AbstractXmlApplicationContext extends AbstractRefreshableApplicationContext{
+
+    @Override
+    protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) {
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory, this);
+        String[] configLocations = getConfigLocations();
+        if (null != configLocations){
+            beanDefinitionReader.loadBeanDefinitions(Arrays.toString(configLocations));
+        }
+    }
+
+    protected abstract String[] getConfigLocations();
+}
