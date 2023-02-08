8f82f9831302e02ec4cc106a8ba5b77ae0a7f345
laogu <1437594522@qq.com>
2023-02-08 16:46:45 +0800

    测试接口

--- spring-step-07/src/test/java/com/lovegu/springframework/bean/UserService.java
+++ spring-step-07/src/test/java/com/lovegu/springframework/bean/UserService.java
@@ -3,11 +3,6 @@ package com.lovegu.springframework.bean;
 import com.lovegu.springframework.beans.factory.DisposableBean;
 import com.lovegu.springframework.beans.factory.InitializingBean;
 
-/**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- */
 public class UserService implements InitializingBean, DisposableBean {
 
     private String uId;
