723a37e61277ce26eec86190a78a349d88406445
laogu <1437594522@qq.com>
2023-02-09 11:10:32 +0800

    容器感知类

---
+++ spring-step-08/src/main/java/com/lovegu/springframework/context/ApplicationContextAware.java
@@ -0,0 +1,11 @@
+package com.lovegu.springframework.context;
+
+import com.lovegu.springframework.beans.factory.Aware;
+
+/**
+ * @author 老顾
+ * @description 感知应用上下文接口
+ * @date 2023/2/9
+ */
+public interface ApplicationContextAware extends Aware {
+}
