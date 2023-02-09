715ccb28e261fe8a52587874788119df2e977b6c
laogu <1437594522@qq.com>
2023-02-09 15:39:42 +0800

    测试感知容器对象接口

--- spring-step-08/src/test/java/com/lovegu/springframework/bean/UserService.java
+++ spring-step-08/src/test/java/com/lovegu/springframework/bean/UserService.java
@@ -1,23 +1,42 @@
 package com.lovegu.springframework.bean;
 
-import com.lovegu.springframework.beans.factory.DisposableBean;
-import com.lovegu.springframework.beans.factory.InitializingBean;
+import com.lovegu.springframework.beans.BeansException;
+import com.lovegu.springframework.beans.factory.*;
+import com.lovegu.springframework.context.ApplicationContext;
+import com.lovegu.springframework.context.ApplicationContextAware;
 
-public class UserService implements InitializingBean, DisposableBean {
+public class UserService implements BeanNameAware, BeanClassLoaderAware, ApplicationContextAware, BeanFactoryAware {
+
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
 
     private String uId;
+
     private String company;
+
     private String location;
+
     private UserDao userDao;
 
     @Override
-    public void destroy() throws Exception {
-        System.out.println("执行：UserService.destroy");
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 
     @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("执行：UserService.afterPropertiesSet");
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name is：" + name);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("ClassLoader：" + classLoader);
     }
 
     public String queryUserInfo() {
@@ -54,6 +73,15 @@ public class UserService implements InitializingBean, DisposableBean {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
     }
 
 
