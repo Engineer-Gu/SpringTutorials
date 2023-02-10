bb343bb4775f512233bd92d201e86ace52b9e99c
laogu <1437594522@qq.com>
2023-02-10 15:49:50 +0800

    初始化提交

---
+++ spring-step-09/src/main/java/com/lovegu/springframework/context/ApplicationContextAware.java
@@ -0,0 +1,14 @@
+package com.lovegu.springframework.context;
+
+import com.lovegu.springframework.beans.BeansException;
+import com.lovegu.springframework.beans.factory.Aware;
+
+/**
+ * @author 老顾
+ * @description 感知应用上下文接口
+ * @date 2023/2/9
+ */
+public interface ApplicationContextAware extends Aware {
+
+    void setApplicationContext(ApplicationContext applicationContext) throws BeansException;
+}
---
+++ spring-step-09/src/test/java/com/lovegu/springframework/bean/UserService.java
@@ -0,0 +1,91 @@
+package com.lovegu.springframework.bean;
+
+import com.lovegu.springframework.beans.BeansException;
+import com.lovegu.springframework.beans.factory.BeanClassLoaderAware;
+import com.lovegu.springframework.beans.factory.BeanFactory;
+import com.lovegu.springframework.beans.factory.BeanFactoryAware;
+import com.lovegu.springframework.beans.factory.BeanNameAware;
+import com.lovegu.springframework.context.ApplicationContext;
+import com.lovegu.springframework.context.ApplicationContextAware;
+
+public class UserService implements BeanNameAware, BeanClassLoaderAware, ApplicationContextAware, BeanFactoryAware {
+
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
+
+    private String uId;
+
+    private String company;
+
+    private String location;
+
+    private UserDao userDao;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name is：" + name);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("ClassLoader：" + classLoader);
+    }
+
+    public String queryUserInfo() {
+        return userDao.queryUserName(uId) + "," + company + "," + location;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+
+}
