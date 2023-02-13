13f26cba3fd652b493b4245e2a5b8468d0186b47
laogu <1437594522@qq.com>
2023-02-13 16:57:46 +0800

    定义拦截注解

---
+++ spring-step-13/src/main/java/com/lovegu/springframework/stereotype/Component.java
@@ -0,0 +1,14 @@
+package com.lovegu.springframework.stereotype;
+
+import java.lang.annotation.*;
+
+/**
+ * @author 老顾
+ * @description 使用 Component 进行自定义注解
+ * @date 2023/2/13
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Component {
+}
