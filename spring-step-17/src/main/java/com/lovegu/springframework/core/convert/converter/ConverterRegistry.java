a527677c7049300464faa0ddf97221d47cfb1e40
laogu <1437594522@qq.com>
2023-02-15 15:57:37 +0800

    定义类型转换接口

---
+++ spring-step-17/src/main/java/com/lovegu/springframework/core/convert/converter/ConverterRegistry.java
@@ -0,0 +1,16 @@
+package com.lovegu.springframework.core.convert.converter;
+
+/**
+ * @author 老顾
+ * @description 类型转换注册接口
+ * @date 2023/2/15
+ */
+public interface ConverterRegistry {
+
+    void addConverter(Converter<?, ?> converter);
+
+    void addConverter(GenericConverter converter);
+
+    void addConverter(ConverterFactory<?, ?> converter);
+
+}
